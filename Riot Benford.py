7cac1376e00c238156e86e4ddad0f9f6c8699a94
Albert <68716258+azlu20@users.noreply.github.com>
2022-01-07 00:37:52 -0600

    Add files via upload

--- Riot Benford.py
+++ Riot Benford.py
@@ -7,25 +7,35 @@ wins = []
 for i in range(1, 10):
     page = str(i)
     response = requests.get(
-        f"https://br1.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/SILVER/I?page={page}&api_key=RGAPI-48a9c0e0-a3bf-4f6b-b242-ac3602906fd8")
+        f"https://br1.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/SILVER/I?page={page}&api_key=RGAPI-ab40f9c6-58c4-427b-8724-062f168fb8a5")
     for ele in response.json():
         cur = ele['wins']
         wins.append(cur)
 print("Max is ", np.max(np.asarray(wins)))
 print("Min is ", np.min(np.asarray(wins)))
 print(len(wins))
+ind = np.arange(9)
+width = 0.3
 first = list(map(lambda x: int(str(x)[0]), wins))
 digits = [i for i in range(1, 10)]
 count = [0 for i in range(1, 10)]
 for ele in first:
     count[ele - 1] += 1
 count = count / np.sum(count)
-y = [0.301, 0.176, 0.125, 0.97, 0.079, 0.067, 0.058, 0.051, 0.046]
+func = lambda a: np.log10(1+1/a)
+y_prime = [func(i) for i in range(1,10)]
+error = np.abs(np.asarray(count) - np.asarray(y_prime))
+error = error/np.asarray(y_prime)
+print(error * 100)
+y = np.linspace(1, 9, 20)
+y = np.log10(y+1) - np.log10(y)
+x = np.linspace(1, 9, 20)
 fit = np.polyfit(np.log(digits), count, 1)
 plt.figure()
-plt.plot(digits, fit[0] * np.log(digits) + fit[1], label= "Log Fit Line")
-plt.bar(digits, count)
-plt.xticks(digits[::1])
+plt.bar(ind, count, width, zorder = 5, label = "Leading Digit from Dataset")
+plt.bar(ind+width, y_prime, width, zorder = 5, label = "Benford's Law Values")
+plt.plot(x-width/0.5, y, c = "r", label= "Benford's Law Curve", zorder = 10)
+plt.xticks(ind+width/2, digits[::1])
 plt.title("First Digit of Number of Wins for Random Players in League of Legend's Silver 1 Elo", fontsize=9)
 plt.ylabel("Frequency")
 plt.xlabel("Digits")
